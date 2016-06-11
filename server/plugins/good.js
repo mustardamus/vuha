a3f1c64c4535b833d3e341286d9b07087c094243
Sebastian Senf <contact@mustardamus.com>
2016-06-11 20:09:07 +0200

    refactored good plugin initilization

--- server/plugins/good.js
+++ server/plugins/good.js
@@ -1,16 +1,20 @@
-if (process.env.NODE_ENV !== 'production') {
-  module.exports = {
-    register: require('good'),
-    options: {
-      reporters: [{
-        reporter: require('good-console'),
-        events: {
-          request: '*',
-          response: '*',
-          log: '*',
-          error: '*'
-        }
-      }]
-    }
+'use strict'
+
+let reporters = [{
+  reporter: require('good-console'),
+  events: {
+    request: '*',
+    response: '*',
+    log: '*',
+    error: '*'
   }
+}]
+
+if (process.env.NODE_ENV === 'production') {
+  reporters = []
+}
+
+module.exports = {
+  register: require('good'),
+  options: { reporters }
 }
