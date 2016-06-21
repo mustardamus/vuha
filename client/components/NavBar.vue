3ed6731f59bed1477fc3e76c97d74846178578be
Sebastian Senf <contact@mustardamus.com>
2016-06-21 11:39:11 +0200

    added buttons and icons to navbar

--- client/components/NavBar.vue
+++ client/components/NavBar.vue
@@ -12,33 +12,48 @@
   <nav class="navbar nav-bar">
     <div class="navbar-left">
       <p class="navbar-item">
-        <a v-link="{ path: '/', exact: true }">
+        <a class="button" v-link="{ path: '/', exact: true }">
           <span class="icon"><i class="fa fa-home"></i></span>
-          Home
+          <span>Home</span>
         </a>
       </p>
 
       <p class="navbar-item" v-if="isAdmin">
-        <a v-link="{ name: 'users' }">Users</a>
+        <a class="button" v-link="{ name: 'users' }">
+          <span class="icon"><i class="fa fa-users"></i></span>
+          <span>Users</span>
+        </a>
       </p>
     </div>
 
     <div class="navbar-right">
       <template v-if="!isLoggedIn">
         <p class="navbar-item">
-          <a v-link="{ name: 'register' }">Register</a>
+          <a class="button" v-link="{ name: 'register' }">
+            <span class="icon"><i class="fa fa-user-plus"></i></span>
+            <span>Register</span>
+          </a>
         </p>
         <p class="navbar-item">
-          <a v-link="{ name: 'login' }">Login</a>
+          <a class="button" v-link="{ name: 'login' }">
+            <span class="icon"><i class="fa fa-sign-in"></i></span>
+            <span>Login</span>
+          </a>
         </p>
       </template>
 
       <template v-if="isLoggedIn">
         <p class="navbar-item">
-          <a v-link="{ name: 'profile' }">Profile</a>
+          <a class="button" v-link="{ name: 'profile' }">
+            <span class="icon"><i class="fa fa-user"></i></span>
+            <span>Profile</span>
+          </a>
         </p>
         <p class="navbar-item">
-          <a @click="onLogoutClick">Logout</a>
+          <a class="button" @click="onLogoutClick">
+            <span class="icon"><i class="fa fa-sign-out"></i></span>
+            <span>Logout</span>
+          </a>
         </p>
       </template>
     </div>
