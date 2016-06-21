0495810823a083770b3f105c3c47c2bac8a845c5
Sebastian Senf <contact@mustardamus.com>
2016-06-21 15:32:15 +0200

    loading overlay styled as a spinning wheel on top center of the page

--- client/components/LoadingOverlay.vue
+++ client/components/LoadingOverlay.vue
@@ -1,16 +1,20 @@
 <style lang="sass">
+.loading-overlay
+  position: absolute
+  top: 0
+  left: 0
+  width: 100%
+  display: none
+  padding-top: 12px
 
+  &.is-active
+    display: block
 </style>
 
 <template>
-  <div class="loading-overlay">
-    <div class="modal {{ isActive ? 'is-active' : ''}}">
-      <div class="modal-background"></div>
-      <div class="modal-container">
-        <div class="modal-content">
-          loading...
-        </div>
-      </div>
+  <div class="loading-overlay {{ isActive ? 'is-active' : ''}}">
+    <div class="has-text-centered">
+      <i class="fa fa-circle-o-notch fa-spin fa-2x fa-fw"></i>
     </div>
   </div>
 </template>
