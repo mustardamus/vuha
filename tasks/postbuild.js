5221c32f1c8f9be8571a0651bd8bdbdb38d8fc6b
Sebastian Senf <contact@mustardamus.com>
2016-06-26 21:41:38 +0200

    added tasks, postbuild task with renaming build.js to md5 name to bust caching, tasks linting

---
+++ tasks/postbuild.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const fs = require('fs')
+const crypto = require('crypto')
+const path = require('path')
+
+let publicPath = path.join(__dirname, '/../public')
+let buildFile = 'build.js'
+let buildPath = publicPath + '/' + buildFile
+let indexPath = publicPath + '/index.html'
+
+if (fs.existsSync(buildPath)) {
+  let buildContent = fs.readFileSync(buildPath, 'utf8')
+  let md5Hash = crypto.createHash('md5').update(buildContent).digest('hex')
+  let md5File = md5Hash + '.js'
+  let indexContent = fs.readFileSync(indexPath, 'utf8')
+
+  fs.renameSync(buildPath, publicPath + '/' + md5File)
+  fs.writeFileSync(indexPath, indexContent.replace(buildFile, md5File), 'utf8')
+
+  console.log(buildFile + ' -> ' + md5File)
+}
